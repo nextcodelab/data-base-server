92324022e15221abeb8e408589635d60c6ad9e52
nextcodelab <73583588+nextcodelab@users.noreply.github.com>
2023-09-05 05:00:50 +0800

    Update appscript.js

--- host_data/bible/appscript/appscript.js
+++ host_data/bible/appscript/appscript.js
@@ -52,7 +52,7 @@ var CRUD = {
     var sheet = SpreadsheetApp.openById(sheetId);
     var numRows = sheet.getLastRow();
 
-    // Delete all rows, starting from row 1 to numRows
+    // return empty string if no data
     if (numRows === 0) {
       return "";
     }
